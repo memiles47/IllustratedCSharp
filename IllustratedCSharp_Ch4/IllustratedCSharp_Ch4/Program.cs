be41c83b4c7f855756c34a67780cc94f98e30172
Michael Miles <boat081062@gmail.com>
2016-03-07 13:18:01 -0600

    Illustrated C# Chapter 4: Sometimes watching videos and typing code isn't quite enough. Reading solidifies those learning examples (Along with typing and stepping through the programs)

---
+++ IllustratedCSharp_Ch4/IllustratedCSharp_Ch4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/*
+Illustrated C# code from chapter 4 illustrating the declaration
+of a class, public instance fields. Creating two instances of the
+same class and initializing the fields within those class instances.
+
+Then accessing those same fields and method and then print to the
+console
+*/
+
+namespace IllustratedCSharp_Ch4
+{
+    class DaysTemp                              //Declare the class
+    {
+        public int high, low;                   //Declare the instance fields
+        public int average()
+        {
+            return (high + low) / 2;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create two instances of DaysTemp
+            DaysTemp T1 = new DaysTemp();
+            DaysTemp T2 = new DaysTemp();
+
+            //Write tow the fields of each instance
+            T1.high = 76;   T1.low = 57;
+            T2.high = 75;   T2.low = 53;
+
+            //Read from the fields of each instance and call a method of
+            //each instance
+            Console.WriteLine($"T1: {T1.high}, {T1.low}, {T1.average()}");
+            Console.WriteLine($"T2: {T2.high}, {T2.low}, {T2.average()}");
+        }
+    }
+}
