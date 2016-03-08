ca06d62a774e1632a42a93d4eb10fbdfe3fb55e6
Michael Miles <boat081062@gmail.com>
2016-03-08 06:22:01 -0600

    Added second example of an array as a formal parameter

---
+++ IllustratedCSharp_Ch5_Fig5.12/IllustratedCSharp_Ch5_Fig5.12/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IllustratedCSharp_Ch5_Fig5._12
+{
+    class MyClass
+    {
+        public void ListInts(params int[] intVals)
+        {
+            if (intVals != null && (intVals.Length != 0))
+            {
+                for (int i = 0; i < intVals.Length; i++)      //Process the array
+                {
+                    intVals[i] *= 10;
+                    Console.WriteLine($"{intVals[i]}");       //Display new values
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 5; int second = 6; int third = 7;     //Declare three ints
+            int[] myArr = new int[] { 7, 8, 9 };
+
+            MyClass mc = new MyClass();                       //Create new MyClass object
+            mc.ListInts(first, second, third);                //Call the method
+            mc.ListInts(myArr);
+
+            Console.WriteLine($"{first}, {second}, {third}"); //Display 
+            foreach (int x in myArr)
+            {
+                Console.WriteLine($"{x}");                    //Display each element
+            }
+        }
+    }
+}
