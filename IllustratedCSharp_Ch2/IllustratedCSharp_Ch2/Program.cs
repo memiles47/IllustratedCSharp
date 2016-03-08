357455eb4f383996f23255cce255f77e282b6c34
Michael E Miles <michael@miles-me.net>
2016-03-07 18:27:44 -0600

    Added new project to demonstrate reference objects passed to a method regular and as 'ref'.

--- IllustratedCSharp_Ch2/IllustratedCSharp_Ch2/Program.cs
+++ IllustratedCSharp_Ch2/IllustratedCSharp_Ch2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IllustratedCSharp_Ch2
 {
@@ -16,7 +12,7 @@ namespace IllustratedCSharp_Ch2
             //CSharp 5.0 text formating (This is compatible in 6.0 as well)
             Console.WriteLine("This is variable one: {0}, This is variable two: {1}", var1, var2);
 
-            //Charp 6.0 text formating (backward compatible of course), my perfered method.
+            //Charp 6.0 text formating, my perfered method.
             //Note: there is a slight difference in line length which all adds up
             Console.WriteLine($"This is variable one: {var1}, This is variable two: {var2}");
             Console.WriteLine("\nBoth versions produce the same results");
---
+++ IllustratedCSharp_Ch5_Fig5.10/IllustratedCSharp_Ch5_Fig5.10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IllustratedCSharp_Ch5_Fig5._10
+{
+    class MyClass { public int val = 20; }
+
+
+    class Program
+    {
+        static void RefAsParameter(ref MyClass f1)
+        {
+            f1.val = 50;
+            Console.WriteLine($"After member assignment:     {f1.val}");
+            f1 = new MyClass();
+            Console.WriteLine($"After new object creation:   {f1.val}");
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass a1 = new MyClass();
+
+            Console.WriteLine($"Before method call:          {a1.val}");
+            RefAsParameter(ref a1);
+            Console.WriteLine($"After method Call:           {a1.val}");
+        }
+    }
+}
