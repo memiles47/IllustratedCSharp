ac8f6d5bca7432fd2f0fe336b446f24cd2aed9ae
Michael Miles <boat081062@gmail.com>
2016-03-10 05:51:59 -0600

    View results of new vs override methods. Predictable once you know how things work.

--- IllustratedCSharp-CodeTesting_Ch7/IllustratedCSharp-CodeTesting_Ch7/SecondDerived.cs
+++ IllustratedCSharp-CodeTesting_Ch7/IllustratedCSharp-CodeTesting_Ch7/SecondDerived.cs
@@ -4,7 +4,7 @@ namespace IllustratedCSharp_CodeTesting_Ch7
 {
     class SecondDerived : MyDerivedClass
     {
-        public override void Print()
+        new public void  Print()
         {
             Console.WriteLine("This is the second derived class.");
         }
